da6e6417302e96c2e2075a07ff4db39dbc4b5016
gpaniati <gpaniati@gmail.com>
2024-07-15 17:52:21 -0300

    Add_Product_Cart v1.1

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,10 +1,12 @@
-//Agrega el producto al carrito seleccionado.
+//Agrega el producto seleccionado al carrito elegido.
 const agregarAlCarrito = ( pid ) => {
-    fetch(`/api/carts/6693166242a6c0ece02f636d/products/${ pid }`, {
+    const carts = document.querySelector('#select-carritos');
+    const cid = carts.options[carts.selectedIndex].value;
+
+    fetch(`/api/carts/${ cid }/products/${ pid }`, {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
-
-        }, 
+        },
     })
 };
